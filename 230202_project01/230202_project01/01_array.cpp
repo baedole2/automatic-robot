99cb7a5ce5eb73ba6ae9c8c41750624f0c7382a1
baedole2 <qodydwls1013@gmail.com>
2023-02-03 18:05:03 +0900

    array 실습

--- 230202_project01/230202_project01/01_array.cpp
+++ 230202_project01/230202_project01/01_array.cpp
@@ -1,67 +1,67 @@
-#include <iostream>
-
-int main() {
-	std::string friend1 = "홍길동";
-	std::string friend2 = "성춘향";
-
-	std::string friends[2];
-	friends[0] = "홍길동";
-	friends[1] = "성춘향";
-	friends[1] = "코딩온";
-
-	int i;
-	i = 0;
-	
-	//int i = 0;
-
-	std::string friends2[2] = { "홍길동" , "성춘향" };
-	// 총갯수가 2개 라는뜻.  but 홍길동은 0번째, 성춘향은 1번째
-
-
-	std::cout << friends2[0];
-
-	int nums[3] = { 1,2,3 };
-	std::cout << nums[2];
-	// 0번째,1번째,"2" 번째 자리인 3을 출력
-
-	double nums2[4] = { 1.5 ,2.5 , 3.5, 4.5 };
-	std::cout << nums2[3];
-	//0번째,1번째,2번째, "3" 번째 자리인 4.5 출력
-
-	//다차원 배열
-	// 2차원 배열
-	std::string fruits1[2][3] = { {"apple" , "grape","1"},{"orange","banana","2"}};
-
-	// 배열의 반복문
-	for (int i = 0; i < 2; i++)	{
-		for (int j = 0; j < 3; j++){
-		std::cout << fruits1[i][j] << std::endl;
-		}
-	}
-
-	//for each 문  지정한 배열만큼 출력을 반복함
-	for (std::string fri : friends) {
-		std::cout << fri << std::endl;
-	}
-
-	//// sizeof() : sizeof 안에 변수 or type을 인자로 넘겨줌. 크기를 byte단위로 구함.
-	//// sizeof(friends)/sizeof(friends[0])
-	//for (int i = 0; i < sizeof(friends) / sizeof(std::string); i++) {
-	//	std::cout << friends[i] << std::endl;
-	//}
-
-		
-
-	// 3차원 배열
-	// 제일 바깥 개수 -> 그다음 안쪽 갯수 -> 제일 안쪽 갯수
-	std::string fruits2[2][3][2] = {
-		{{"a1","b1"}, {"c1","d1"}, {"e1","f1"}},
-		{{"a2","b2"}, {"c2","d2"}, {"e2","f2"}}
-	};
-
-
-	std::cout << fruits2[0][1][0] << std::endl;
-
-
-
-}
+//#include <iostream>
+//
+//int main() {
+//	std::string friend1 = "홍길동";
+//	std::string friend2 = "성춘향";
+//
+//	std::string friends[2];
+//	friends[0] = "홍길동";
+//	friends[1] = "성춘향";
+//	friends[1] = "코딩온";
+//
+//	int i;
+//	i = 0;
+//	
+//	//int i = 0;
+//
+//	std::string friends2[2] = { "홍길동" , "성춘향" };
+//	// 총갯수가 2개 라는뜻.  but 홍길동은 0번째, 성춘향은 1번째
+//
+//
+//	std::cout << friends2[0];
+//
+//	int nums[3] = { 1,2,3 };
+//	std::cout << nums[2];
+//	// 0번째,1번째,"2" 번째 자리인 3을 출력
+//
+//	double nums2[4] = { 1.5 ,2.5 , 3.5, 4.5 };
+//	std::cout << nums2[3];
+//	//0번째,1번째,2번째, "3" 번째 자리인 4.5 출력
+//
+//	//다차원 배열
+//	// 2차원 배열
+//	std::string fruits1[2][3] = { {"apple" , "grape","1"},{"orange","banana","2"}};
+//
+//	// 배열의 반복문
+//	for (int i = 0; i < 2; i++)	{
+//		for (int j = 0; j < 3; j++){
+//		std::cout << fruits1[i][j] << std::endl;
+//		}
+//	}
+//
+//	//for each 문  지정한 배열만큼 출력을 반복함
+//	for (std::string fri : friends) {
+//		std::cout << fri << std::endl;
+//	}
+//
+//	// sizeof() : sizeof 안에 변수 or type을 인자로 넘겨줌. 크기를 byte단위로 구함.
+//	// sizeof(friends)/sizeof(friends[0])
+//	for (int i = 0; i < sizeof(friends) / sizeof(std::string); i++) {
+//		std::cout << friends[i] << std::endl;
+//	}
+//
+//		
+//
+//	// 3차원 배열
+//	// 제일 바깥 개수 -> 그다음 안쪽 갯수 -> 제일 안쪽 갯수
+//	std::string fruits2[2][3][2] = {
+//		{{"a1","b1"}, {"c1","d1"}, {"e1","f1"}},
+//		{{"a2","b2"}, {"c2","d2"}, {"e2","f2"}}
+//	};
+//
+//
+//	std::cout << fruits2[0][1][0] << std::endl;
+//
+//
+//
+//}
---
+++ 230202_project01/230202_project01/02_Practice.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main()
+{
+	////실습1 배열 사용해보기
+	////city라는 배열 변수를 만들고, 5개의 나라 이름을 값으로 지정
+	////for문 혹은 for-each문을 활용하여 city 안의 모든 워소를 출력
+
+	//std::string nation[] = {"Korea","Japan", "U.S.A","Ukraine","Polland"};
+
+	//for (std::string nat : nation){
+	//	std::cout << nat << std::endl;
+	//}
+
+
+	//실습2 배열 활용하기
+	//grade라는 배열 변수를 선언하고, 학생 5인의 성적 입력받기
+	//for문 혹은 for-each문을 활용하여 학생 5인의 성적 평균 산출
+
+	std::string student[] = { "믹키유천","영웅재중","유노윤호","최강창민","시아준수" };
+	double score[5] = { };
+	double totalScore =0;
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << student[i] << " 학생의 점수를 입력하세요. : ";
+		std::cin >> score[i];
+		std::cout << std::endl;
+	}
+
+	for (double sc : score) {
+		totalScore = totalScore + sc;
+	}
+
+	// totalScore/5  이부분을 숫자 5 대신 학생 수의 값을 따오고 싶은데 어떻게하면 좋을까요?
+	std::cout << "성적 평균 : " << totalScore / 5;
+}
