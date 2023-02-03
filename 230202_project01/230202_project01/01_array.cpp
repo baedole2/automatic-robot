db4f2991b48dee7cdb3b776e557d3098808c53e0
baedole2 <qodydwls1013@gmail.com>
2023-02-03 17:21:14 +0900

    배열 선언2

--- 230202_project01/230202_project01/01_array.cpp
+++ 230202_project01/230202_project01/01_array.cpp
@@ -25,13 +25,32 @@ int main() {
 	// 0번째,1번째,"2" 번째 자리인 3을 출력
 
 	double nums2[4] = { 1.5 ,2.5 , 3.5, 4.5 };
-	std::cout << nums[3];
+	std::cout << nums2[3];
 	//0번째,1번째,2번째, "3" 번째 자리인 4.5 출력
 
 	//다차원 배열
 	// 2차원 배열
 	std::string fruits1[2][3] = { {"apple" , "grape","1"},{"orange","banana","2"}};
 
+	// 배열의 반복문
+	for (int i = 0; i < 2; i++)	{
+		for (int j = 0; j < 3; j++){
+		std::cout << fruits1[i][j] << std::endl;
+		}
+	}
+
+	//for each 문  지정한 배열만큼 출력을 반복함
+	for (std::string fri : friends) {
+		std::cout << fri << std::endl;
+	}
+
+	//// sizeof() : sizeof 안에 변수 or type을 인자로 넘겨줌. 크기를 byte단위로 구함.
+	//// sizeof(friends)/sizeof(friends[0])
+	//for (int i = 0; i < sizeof(friends) / sizeof(std::string); i++) {
+	//	std::cout << friends[i] << std::endl;
+	//}
+
+		
 
 	// 3차원 배열
 	// 제일 바깥 개수 -> 그다음 안쪽 갯수 -> 제일 안쪽 갯수
@@ -39,5 +58,10 @@ int main() {
 		{{"a1","b1"}, {"c1","d1"}, {"e1","f1"}},
 		{{"a2","b2"}, {"c2","d2"}, {"e2","f2"}}
 	};
+
+
+	std::cout << fruits2[0][1][0] << std::endl;
+
+
 
 }
