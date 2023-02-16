8532eb187d5bf70ceb6d8ae9e9822f8ce49a3ee0
baedole2 <qodydwls1013@gmail.com>
2023-02-16 14:48:40 +0900

    실습2 타임어택끝말잇기 게임

--- 230215_project01/230215_project01/01_Practice01.cpp
+++ 230215_project01/230215_project01/01_Practice01.cpp
@@ -1,63 +1,63 @@
-// 프로젝트1 배스킨라빈스 31 게임
-// 서로 최소 1개에서 최대 3개씩 연속된 숫자를 말하고
-// 31 이라는 숫자를 말하는 사람이 지는 게임.
-//		1) 사용자는 본인이 입력한 숫자만큼.
-//		2) 컴퓨터는 랜덤 숫자만큼.
-//		3) 단 사용자, 컴퓨터 모둗 1~3사이의 숫자만 가능!
-
-#include <iostream>
-#include <cstdlib>
-
-using std::cout;
-using std::cin;
-using std::endl;
-
-int main() {
-	std::srand(time(NULL));	// 랜덤 난수
-
-	int input;
-	int num = 0;
-	bool isGameOver = false;
-	bool isPlayerLose = false;
-	bool isPlayerTurn = true;
-
-	cout << "배스킨 라빈스 31 게임" << endl;
-
-	while (isGameOver == false) {
-		if (isPlayerTurn == true) {
-			cout << "--------------------" << endl;
-			cout << "숫자를 입력하세요 ( 1-3 ) : ";
-			cin >> input;
-
-			if (input < 1 && input > 3) {
-				cout << "입력값을 벗어났습니다. 1-3 사이의 숫자만 입력하세요." << endl;
-				continue;
-			}
-		}
-		else
-			input = rand() % 3 + 1;
-
-		(isPlayerTurn) ? cout << "사용자" : cout << "컴퓨터";
-		cout << " 가 숫자 " << input << "개를 선언했습니다." << endl;
-		for (int i = 0; i < input; i++) {
-			cout << ++num << " ";
-			if (num == 31 && isPlayerTurn == true) {
-				isGameOver = true;
-				isPlayerLose = true;
-				break;
-			}
-			else if (num == 31 && isPlayerTurn == false) {
-				isGameOver = true;
-				isPlayerLose = false;
-				break;
-			}
-		}
-		cout << endl;
-		if (isGameOver == true)	// 게임종료 만족시 탈출
-			break;
-		isPlayerTurn = !isPlayerTurn;
-	}
-	(isPlayerLose == true) ?
-		cout << endl << "게임 종료. 컴퓨터 의 승리입니다." << endl
-		: cout << endl << "게임 종료. 사용자 의 승리입니다." << endl;
-}
+//// 프로젝트1 배스킨라빈스 31 게임
+//// 서로 최소 1개에서 최대 3개씩 연속된 숫자를 말하고
+//// 31 이라는 숫자를 말하는 사람이 지는 게임.
+////		1) 사용자는 본인이 입력한 숫자만큼.
+////		2) 컴퓨터는 랜덤 숫자만큼.
+////		3) 단 사용자, 컴퓨터 모둗 1~3사이의 숫자만 가능!
+//
+//#include <iostream>
+//#include <cstdlib>
+//
+//using std::cout;
+//using std::cin;
+//using std::endl;
+//
+//int main() {
+//	std::srand(time(NULL));	// 랜덤 난수
+//
+//	int input;
+//	int num = 0;
+//	bool isGameOver = false;
+//	bool isPlayerLose = false;
+//	bool isPlayerTurn = true;
+//
+//	cout << "배스킨 라빈스 31 게임" << endl;
+//
+//	while (isGameOver == false) {
+//		if (isPlayerTurn == true) {
+//			cout << "--------------------" << endl;
+//			cout << "숫자를 입력하세요 ( 1-3 ) : ";
+//			cin >> input;
+//
+//			if (input < 1 && input > 3) {
+//				cout << "입력값을 벗어났습니다. 1-3 사이의 숫자만 입력하세요." << endl;
+//				continue;
+//			}
+//		}
+//		else
+//			input = rand() % 3 + 1;
+//
+//		(isPlayerTurn) ? cout << "사용자" : cout << "컴퓨터";
+//		cout << " 가 숫자 " << input << "개를 선언했습니다." << endl;
+//		for (int i = 0; i < input; i++) {
+//			cout << ++num << " ";
+//			if (num == 31 && isPlayerTurn == true) {
+//				isGameOver = true;
+//				isPlayerLose = true;
+//				break;
+//			}
+//			else if (num == 31 && isPlayerTurn == false) {
+//				isGameOver = true;
+//				isPlayerLose = false;
+//				break;
+//			}
+//		}
+//		cout << endl;
+//		if (isGameOver == true)	// 게임종료 만족시 탈출
+//			break;
+//		isPlayerTurn = !isPlayerTurn;
+//	}
+//	(isPlayerLose == true) ?
+//		cout << endl << "게임 종료. 컴퓨터 의 승리입니다." << endl
+//		: cout << endl << "게임 종료. 사용자 의 승리입니다." << endl;
+//}
--- 230215_project01/230215_project01/02_Practice02.cpp
+++ 230215_project01/230215_project01/02_Practice02.cpp
@@ -1,24 +1,53 @@
-//// 프로젝트2 타임어택 끝말잇기 게임
-//// 1) 제한 시간 30초 동안 플레이어는 끝말잇기 규칙에 따라 단어를 입력
-//// 2) 제한 시간이 종료되면 게임을 종료 시키고, 입력한 단어의 개수를 출력
-//
-//#include <iostream>
-//#include <ctime>	//시간 측정을 위해서
-//
-//using std::cout;
-//using std::cin;
-//using std::endl;
-//
-//
-//int main() {
-//	// 1) 제한 시간 30초 동안 플레이어는 끝말잇기 규칙에 따라 단어를 입력
-//	// 앞 단어의 마지막 문자로 시작하는 단어여야 한다.
-//	// 같은 단어는 쓸 수 없다.
-//	// *3번 틀리면 그 즉시 탈락
-//	// 2) 제한 시간이 종료되면 게임을 종료 시키고, 입력한 단어의 개수를 출력
-//
-//	clock_t startTime = clock();
-//
-//	clock_t endTime = clock();
-//	cout << "걸린 시간 : " << (endTime - startTime) / CLOCKS_PER_SEC << endl;
-//}
+// 프로젝트2 타임어택 끝말잇기 게임
+// 1) 제한 시간 30초 동안 플레이어는 끝말잇기 규칙에 따라 단어를 입력
+// 2) 제한 시간이 종료되면 게임을 종료 시키고, 입력한 단어의 개수를 출력
+
+#include <iostream>
+#include <ctime>	//시간 측정을 위해서
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main() {
+	int word_count = 0;
+	int wrong_count = 0;
+	string input;
+	string word = "airplane";
+	string word_save = "airplane";	// 쭉 보여주는 칸
+	bool isGameOver = false;
+
+	cout << "끝말잇기 게임" << endl;
+	while (isGameOver == false) {
+		clock_t startTime = clock();
+		cout << word_save << endl;	// 단어 모음 계속 보여줌
+		cout << "다음 단어를 입력하세요. : ";
+		cin >> input;
+
+		int word_size = word.size();
+		if (word[word_size - 1] == input[0]) {// 제시어의 제일 끝과 입력의 제일 앞부분 일치 비교
+			word = input;
+			word_save += "->" + input;
+			word_count++;
+		}
+		else {
+			cout << "잘못된 입력입니다. " << endl << endl;
+			wrong_count++;
+			(wrong_count >= 3) ? isGameOver = true : isGameOver = false;
+			continue;
+		}
+
+		clock_t endTime = clock();
+		int elapsedTime = (endTime - startTime) / CLOCKS_PER_SEC;
+		cout << "입력까지 걸린 시간 : " << elapsedTime << endl << endl;
+
+		if (elapsedTime >= 30) {	// 시간초과 패배
+			cout << "30초를 초과하였습니다. 타임 오버!" << endl;
+			isGameOver = true;
+			continue;
+		}
+	}
+
+	cout << "게임 종료!" << endl << "입력한 총 단어 갯수 : " << word_count;
+}
