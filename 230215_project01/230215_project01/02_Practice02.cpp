be43b4bc1e6b365c34fc9c8c695334e50e76891a
baedole2 <qodydwls1013@gmail.com>
2023-02-16 17:29:15 +0900

    실습3 랜덤숫자 맞추기

--- 230215_project01/230215_project01/02_Practice02.cpp
+++ 230215_project01/230215_project01/02_Practice02.cpp
@@ -1,53 +1,53 @@
-// 프로젝트2 타임어택 끝말잇기 게임
-// 1) 제한 시간 30초 동안 플레이어는 끝말잇기 규칙에 따라 단어를 입력
-// 2) 제한 시간이 종료되면 게임을 종료 시키고, 입력한 단어의 개수를 출력
-
-#include <iostream>
-#include <ctime>	//시간 측정을 위해서
-
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
-
-int main() {
-	int word_count = 0;
-	int wrong_count = 0;
-	string input;
-	string word = "airplane";
-	string word_save = "airplane";	// 쭉 보여주는 칸
-	bool isGameOver = false;
-
-	cout << "끝말잇기 게임" << endl << "30초 안에 마구마구 영단어를 입력하세요!" << endl;
-	clock_t startTime = clock();
-	while (isGameOver == false) {
-
-		cout << word_save << endl;	// 단어 모음 계속 보여줌
-		cout << "다음 단어를 입력하세요. : ";
-		cin >> input;
-
-		clock_t endTime = clock();
-		int elapsedTime = (endTime - startTime) / CLOCKS_PER_SEC;
-		cout << "입력까지 걸린 시간 : " << elapsedTime << endl << endl;
-
-		if (elapsedTime >= 30) {	// 시간초과 패배
-			cout << "30초를 초과하였습니다. 타임 오버!" << endl;
-			isGameOver = true;
-			continue;
-		}
-		int word_size = word.size();
-		if (word[word_size - 1] == input[0]) {// 제시어의 제일 끝과 입력의 제일 앞부분 일치 비교
-			word = input;
-			word_save += "->" + input;
-			word_count++;
-		}
-		else {
-			cout << "잘못된 입력입니다. " << endl << endl;
-			wrong_count++;
-			(wrong_count >= 3) ? isGameOver = true : isGameOver = false;
-			continue;
-		}
-	}
-
-	cout << "게임 종료!" << endl << "입력한 총 단어 갯수 : " << word_count;
-}
+//// 프로젝트2 타임어택 끝말잇기 게임
+//// 1) 제한 시간 30초 동안 플레이어는 끝말잇기 규칙에 따라 단어를 입력
+//// 2) 제한 시간이 종료되면 게임을 종료 시키고, 입력한 단어의 개수를 출력
+//
+//#include <iostream>
+//#include <ctime>	//시간 측정을 위해서
+//
+//using std::cout;
+//using std::cin;
+//using std::endl;
+//using std::string;
+//
+//int main() {
+//	int word_count = 0;
+//	int wrong_count = 0;
+//	string input;
+//	string word = "airplane";
+//	string word_save = word;	// 쭉 보여주는 칸
+//	bool isGameOver = false;
+//
+//	cout << "끝말잇기 게임" << endl << "30초 안에 마구마구 영단어를 입력하세요!" << endl;
+//	clock_t startTime = clock();
+//	while (isGameOver == false) {
+//
+//		cout << word_save << endl;	// 단어 모음 계속 보여줌
+//		cout << "다음 단어를 입력하세요. : ";
+//		cin >> input;
+//
+//		clock_t endTime = clock();
+//		int elapsedTime = (endTime - startTime) / CLOCKS_PER_SEC;
+//		cout << "입력까지 걸린 시간 : " << elapsedTime << endl << endl;
+//
+//		if (elapsedTime >= 30) {	// 시간초과 패배
+//			cout << "30초를 초과하였습니다. 타임 오버!" << endl;
+//			isGameOver = true;
+//			continue;
+//		}
+//		int word_size = word.size();
+//		if (word[word_size - 1] == input[0]) {// 제시어의 제일 끝과 입력의 제일 앞부분 일치 비교
+//			word = input;
+//			word_save += "->" + input;
+//			word_count++;
+//		}
+//		else {
+//			cout << "잘못된 입력입니다. " << endl << endl;
+//			wrong_count++;
+//			(wrong_count >= 3) ? isGameOver = true : isGameOver = false;
+//			continue;
+//		}
+//	}
+//
+//	cout << "게임 종료!" << endl << "입력한 총 단어 갯수 : " << word_count;
+//}
--- 230215_project01/230215_project01/03_Practice03.cpp
+++ 230215_project01/230215_project01/03_Practice03.cpp
@@ -1,29 +1,72 @@
-//// 프로젝트3 랜덤 숫자 맞추기
-//// 1) 컴퓨터가 1~25까지의 수 중 랜덤으로 6개의 수 지정 (중복 x)
-//// 2) 사용자도 1~25까지의 수 중 원하는 숫자 6개 입력 (중복 x)
-//// 3) 두 배열을 비교해서 몇 등인지 출력하기!
-////		3-1) 한 개도 맞추지 못하면 7등, 1개만 맞추면 6등, 2개 맞추면 5등...
-////		3-2) 6개를 모두 맞추면 1등
-//
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//
-//	int input;
-//
-//
-//
-//	cout << "숫자는 1 ~25 까지의 숫자만 입력할 수 있습니다." << endl;
-//
-//	for (int i = 0; i < 6; i++)
-//	{
-//		cout << i << "번째 번호를 입력하세요. : ";
-//		cin >> input;
-//
-//
-//
-//
-//	}
-//}
+// 프로젝트3 랜덤 숫자 맞추기
+// 1) 컴퓨터가 1~25까지의 수 중 랜덤으로 6개의 수 지정 (중복 x)
+// 2) 사용자도 1~25까지의 수 중 원하는 숫자 6개 입력 (중복 x)
+// 3) 두 배열을 비교해서 몇 등인지 출력하기!
+//		3-1) 한 개도 맞추지 못하면 7등, 1개만 맞추면 6등, 2개 맞추면 5등...
+//		3-2) 6개를 모두 맞추면 1등
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+int main() {
+
+	int input;
+	int random_num;
+	int prize = 0;
+	vector<int> num;
+	vector<int> com_num;
+
+	std::srand(time(NULL));
+
+	cout << "숫자는 1 ~ 25 까지의 숫자만 입력할 수 있습니다." << endl;
+
+	for (int i = 0; i < 6; i++) {
+		cout << i+1 << "번째 번호를 입력하세요. : ";
+		cin >> input;
+		if(input >=1 && input <= 25)
+		num.push_back(input);
+		else {
+			cout << "잘못된 입력입니다. 다시 입력해주세요." << endl;
+			i--;
+			continue;
+		}
+
+		for (int j = 0; j < i; j++) {
+			if (num[i] == num[j]) {	// 만든수를 중복검사_사용자
+				cout << "이미 입력된 숫자입니다. 다시 입력해주세요." << endl;
+				num.pop_back();
+				i--;
+			}
+		}
+	}
+
+	for (int i = 0; i < 6; i++) {
+		random_num = std::rand() % 25 + 1;	//	난수생성
+		com_num.push_back(random_num);
+		for (int j = 0; j < i; j++) {
+			if (com_num.at(i) == com_num.at(j)) {	// 만든수를 중복검사_컴퓨터
+				com_num.pop_back();
+				i--;
+			}
+		}
+	}
+
+	cout << "당첨번호 공개!" << endl;
+
+	for (int i = 0; i < num.size(); i++) {
+		for (int j = 0; j < com_num.size(); j++) {
+			if (num[i] == com_num[j])
+				prize++;
+		}
+		cout << com_num[i] << " ";
+	}
+	cout << "\n1 ~ 7등까지 결과가 나올 수 있습니다." << endl;
+	cout << "결과: " << prize << "개의 숫자가 일치합니다. " << 7 - prize << "등 입니다!";
+}
